c05acdbc65ac5f408c75e7ade3936f9af3c21c9a
chenyucheng05 <chenyucheng05@meituan.com>
2023-07-24 11:38:19 +0800

    add group_id feature

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ import pickle
 import logging
 from feedgen.feed import FeedGenerator
 from telethon.tl.types import InputPeerChannel, MessageMediaPhoto, MessageMediaDocument
-
+from uvicorn import run
 import boto3
 
 config = configparser.ConfigParser()
@@ -18,7 +18,7 @@ config.read('config.ini')
 
 app = FastAPI()
 
-block_list = ["广告", "证书", "机场", "流量", "游戏", "运营商", "代理", "星链云"]
+block_list = ["证书", "机场", "流量卡", "游戏", "运营商", "星链云"]
 
 try:
     with open('hash.pickle', 'rb') as f:
@@ -40,6 +40,22 @@ def upload_pictures(file_path, object_name):
     except Exception as e:
         print(f"上传失败：{e}")
         return False
+
+
+async def parse_photo_document(client, messages, channel_name):
+    message_photo_content = ""
+    for message in messages:
+        if (message.media and isinstance(message.media, MessageMediaPhoto)) or isinstance(message.media, MessageMediaDocument):
+            file_name = "{}_{}.jpg".format(channel_name, str(message.id))
+            file_path = "{}/pictures/{}".format(os.getcwd(), file_name)
+            if os.path.exists(file_path):
+                picture_url = config['PICTURES']['END_POINT'] + "/" + config['PICTURES']['BUCKET_NAME'] + "/" + file_name
+            else:
+                await client.download_media(message, thumb=-1, file=file_path)
+                picture_url = upload_pictures(file_path, file_name)
+
+            message_photo_content += f'<a href="{picture_url}" target="_blank" rel="noopener" onclick="return confirm(\'Open this link? Click OK to open:{picture_url}\');"><img src="{picture_url}" alt="Image"></a>'
+    return message_photo_content
 
 @app.get("/")
 def read_root():
@@ -74,46 +90,40 @@ async def channels(channel: str):
                 fg.generator(config['RSS']['GENERATOR'])
 
                 message_count = int(config['RSS']['RECORDS'])
-                # print(ch['username'])
+                message_id = []
                 async for message in client.iter_messages(ch['username']):
-                    text = message.text
                     if message_count < 0:
                         break
-                    if text and any(item in text for item in block_list):
+                    if message.id in message_id:
+                        # 可能group_id中处理过这个message了
+                        continue
+                    message_id.append(message.id)
+
+                    message_group = [message]
+                    message_group_text = str(markdown(message.text))
+                    if message.grouped_id:
+                        group_flag = True
+                        curr_id = message.id
+                        while True:
+                            next_message = await client.get_messages(ch['username'], ids=curr_id - 1)
+                            if next_message.grouped_id and next_message.grouped_id == message.grouped_id:
+                                message_group.append(next_message)
+                                message_id.append(next_message.id)
+                                message_group_text += str(markdown(next_message.text))
+                                curr_id -= 1
+                            else:
+                                break
+                    if message_group_text and any(item in message_group_text for item in block_list):
                         continue
 
-                    message_content = ""
-                    if message.media and isinstance(message.media, MessageMediaPhoto):
-                        photo = message.media.photo
-                        file_name = "{}_{}.jpg".format(ch['username'], str(message.id))
-                        file_path = "/root/code/tgtorss/pictures/{}".format(file_name)
-                        if os.path.exists(file_path):
-                            picture_url = config['PICTURES']['END_POINT'] + "/" + config['PICTURES']['BUCKET_NAME'] + "/" + file_name
-                        else:
-                            await client.download_media(message, thumb=-1, file=file_path)
-                            picture_url = upload_pictures(file_path, file_name)
-
-                        message_content += f'<a href="{picture_url}" target="_blank" rel="noopener" onclick="return confirm(\'Open this link? Click OK to open:{picture_url}\');"><img src="{picture_url}" alt="Image"></a>'
-                        # message_content += "![{}]({})".format(file_name, picture_url)
-
-                    if isinstance(message.media, MessageMediaDocument):
-                        file_name = "{}_{}_thumb.jpg".format(ch['username'], str(message.id))
-                        file_path = "/root/code/tgtorss/pictures/{}".format(file_name)
-                        if os.path.exists(file_path):
-                            picture_url = config['PICTURES']['END_POINT'] + "/" + config['PICTURES']['BUCKET_NAME'] + "/" + file_name
-                        else:
-                            await client.download_media(message, thumb=-1, file=file_path)
-                            picture_url = upload_pictures(file_path, file_name)
-                        message_content += f'<a href="{picture_url}" target="_blank" rel="noopener" onclick="return confirm(\'Open this link? Click OK to open:{picture_url}\');"><img src="{picture_url}" alt="Image"></a>'
-                        # message_content += "![{}]({})".format(file_name, picture_url)
+                    message_photo_content = await parse_photo_document(client, message_group, ch['username'])
 
                     # print(message.id, message.text)
-                    if text or message_content != "":
-                        message_content += str(markdown(message.text))
+                    if message_group_text!="" or message_photo_content != "":
                         fe = fg.add_entry(order='append')
                         fe.title(markdown(message.text).strip().splitlines()[0])
                         fe.guid(guid=f"{link}{ch['username']}/{message.id}", permalink=True)
-                        fe.content(message_content)
+                        fe.content(message_photo_content + message_group_text)
                         fe.published(message.date)
                     message_count -= 1
                 return fg.rss_str()
@@ -126,4 +136,5 @@ async def channels(channel: str):
         logging.error(warn)
         return warn
 
-
+if __name__ == '__main__':
+    run(app)
